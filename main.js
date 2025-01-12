7805aa845ebc58b94dd1ac9820e85e363aedce69
strannosti <108225468+strannosti@users.noreply.github.com>
2025-01-12 23:58:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("orderForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const order = document.getElementById("order").value;
+
+
+    const message = `Заказ от: ${name}%0AТелефон: ${phone}%0AДетали заказа:%0A${order}`;
+
+    const whatsappURL = `https://wa.me/996500558415?text=${message}`; 
+
+   
+    window.open(whatsappURL, "_blank");
+  });
